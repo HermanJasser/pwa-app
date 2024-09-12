000b4614c7c4b719143fa3c0a4693818d69fb01d
HermanJasser <herjas00989@stud.noroff.no>
2024-09-12 15:08:17 +0200

    ferdig

--- utils/math.js
+++ utils/math.js
@@ -1,6 +1,41 @@
 export function showMathUtility(container) {
     container.innerHTML = `
+    <div class="calc">
         <p>Math Utilities:</p>
-        <p>2 + 2 = ${2 + 2}</p>
-        <p>2 * 3 = ${2 * 3}</p>
-`; }
+        <input id="num1" type="number" placeholder="number 1">
+        <input id="num2" type="number" placeholder="number 2">
+        <button id="mul">*</button>
+        <button id="div">/</button>
+        <button id="sub">-</button>
+        <button id="add">+</button>
+        <p id="output"></p>
+        </div>
+`; 
+
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let output = document.getElementById("output");
+
+document.getElementById("mul").addEventListener("click", function(){
+    output.innerHTML = parseInt(num1.value) * parseInt(num2.value);
+
+    })
+
+document.getElementById("div").addEventListener("click", function(){
+    output.innerHTML = parseInt(num1.value) / parseInt(num2.value);
+    })
+
+document.getElementById("sub").addEventListener("click", function(){
+    output.innerHTML = parseInt(num1.value) - parseInt(num2.value);
+    })
+document.getElementById("add").addEventListener("click", function(){
+    output.innerHTML = parseInt(num1.value) + parseInt(num2.value);
+    })
+}
+
+
+
+
+
+
+
